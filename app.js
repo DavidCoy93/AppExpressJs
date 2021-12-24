5b8e6c1c10ae4b1883fcb2437640a35eed156928
David Alvarado <david-coy@hotmail.com>
2021-12-24 14:07:28 -0600

    Cambios navideños

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', router);
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname));
+app.listen(1666, () => {
+    console.log("El servidor se esta inicializando en el puerto 1666");
+});
 
 let usersData = fs.readFileSync(path.join(__dirname, '/Usuarios.json'));
 let ObjUsers = JSON.parse(usersData);
@@ -23,42 +34,27 @@ router.get('/personas', function(req, res){
     res.send(ObjUsers);
 });
 
-var jsonParser = bodyParser.json();
-
-router.post('/iniciar', jsonParser, function(req, res){
+router.post('/iniciar', function(req, res){
     console.log(req.body);
+    let ObjPersona = {
+        User: req.body.User,
+        Pass: req.body.Pass,
+        Existe: false
+    };
     for (let i = 0; i < ObjUsers.Usuarios.length; i++) {
         if (ObjUsers.Usuarios[i].Nombre === req.body.User && ObjUsers.Usuarios[i].Password === req.body.Pass && ObjUsers.Usuarios[i].VaAPistearHoy === true) {
-            console.log("Entre al if");
-            var ObjPersonaCorrecto = {
-                User: req.body.User,
-                Pass: req.body.Pass,
-                Existe: true
-            };
-            res.send(ObjPersonaCorrecto);
-            console.log("Acceso correcto");
+            ObjPersona.Existe = true;
             break;
         } else {
-            console.log("No entre al if");
-            var ObjPersonaError = {
-                User: null,
-                Pass: null,
-                Existe: false
-            };
-            res.send(ObjPersonaError);
-            console.log("El usuario no existe");
+            ObjPersona.Existe = false;
+            console.log("iteracion No: " + i);
         }
     }
+    if (ObjPersona.Existe === true) {
+        console.log("Acceso correcto");
+    } else {
+        console.log("El usuario no existe");
+    }
+    res.send(ObjPersona);
 });
 
-app.use('/', router);
-
-app.use(cors({
-    origin: '*'
-}));
-
-app.use(express.static(__dirname + '/css'));
-
-app.listen(1666, () => {
-    console.log("El servidor se esta inicializando en el puerto 1666");
-});
